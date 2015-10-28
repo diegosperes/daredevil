20347d0f5b543a9f39ced0411ed9cc02e007d777
Diego.Péres <speres.diego@gmail.com>
2015-10-28 09:14:53 -0200

    Corrigindo identação do template javascript.

--- daredevil/templates/javascript/default.js
+++ daredevil/templates/javascript/default.js
@@ -1,45 +1,45 @@
 {% include 'javascript/annyang.js' %}
 
 var mostrarEsconder = function(seletorCSS){
-	$(seletorCSS).toggle();
+  $(seletorCSS).toggle();
 }
 
 var ler = function(seletorCSS){
-	var elemento = $(seletorCSS);
-	var mensagem = '';
+  var elemento = $(seletorCSS);
+  var mensagem = '';
 
-	if (elemento.is(':visible')){
-		mensagem = $(seletorCSS).text();
-	}
+  if (elemento.is(':visible')){
+    mensagem = $(seletorCSS).text();
+  }
 
-	if (window.SpeechSynthesisUtterance !== undefined) {
+  if (window.SpeechSynthesisUtterance !== undefined) {
 
-		var sinteseVoz = new SpeechSynthesisUtterance();
-		var voz = window.speechSynthesis.getVoices();
-		sinteseVoz.voice = voz[10];
-		sinteseVoz.voiceURI = 'native';
-		sinteseVoz.volume = 1;
-		sinteseVoz.rate = 1;
-		sinteseVoz.pitch = 2;
-		sinteseVoz.text = mensagem;
-		sinteseVoz.lang = 'pt-BR';
+    var sinteseVoz = new SpeechSynthesisUtterance();
+    var voz = window.speechSynthesis.getVoices();
+    sinteseVoz.voice = voz[10];
+    sinteseVoz.voiceURI = 'native';
+    sinteseVoz.volume = 1;
+    sinteseVoz.rate = 1;
+    sinteseVoz.pitch = 2;
+    sinteseVoz.text = mensagem;
+    sinteseVoz.lang = 'pt-BR';
 
-		speechSynthesis.speak(sinteseVoz);
-	}
+    speechSynthesis.speak(sinteseVoz);
+  }
 }
 
 var initialize = function(){
-	var commands = {
-		{% for comando in comandos %}
-			'{{ comando.regex }}': {{ processa_calback_comando(comando) }},
-		{% endfor %}
-	}
+  var commands = {
+    {% for comando in comandos %}
+      '{{ comando.regex }}': {{ processa_calback_comando(comando) }},
+    {% endfor %}
+  }
 
-	if (annyang){
-		annyang.addCommands(commands);
-		annyang.setLanguage('pt');
-		annyang.start();
-	}
+  if (annyang){
+    annyang.addCommands(commands);
+    annyang.setLanguage('pt');
+    annyang.start();
+  }
 }
 
 document.onreadystatechange = function () {
